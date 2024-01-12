642d3dfb71474452ffb698128ca17307cba02e4a
pmart1n <123256191+pmart1n@users.noreply.github.com>
2024-01-12 23:20:17 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,48 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function newCard() {
+  let palos = ["♦", "♥", "♠", "♣"];
+  let numeros = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let numeroPalos = Math.floor(Math.random() * palos.length);
+  let numeroAleatorio = Math.floor(Math.random() * numeros.length);
+  console.log(palos[numeroPalos], numeros[numeroAleatorio]);
+  let top = document.querySelector("#top");
+  let bot = document.querySelector("#bot");
+  let number = document.querySelector("#number");
+  top.innerHTML = palos[numeroPalos];
+  bot.innerHTML = palos[numeroPalos];
+  number.innerHTML = numeros[numeroAleatorio];
+  let currentClass =
+    palos[numeroPalos] == "♦" || palos[numeroPalos] == "♥"
+      ? "text-danger"
+      : "text-dark";
+  // palos == "♥" ? "text-danger" : "text-dark";
+  top.className = currentClass;
+  bot.className = currentClass;
+  number.className = currentClass;
+
+  setInterval(() => newCard(), 5000);
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  newCard();
 };
+
+let btnNuevaCarta = document.querySelector("#BtnNewCard");
+btnNuevaCarta.addEventListener("click", () => newCard());
